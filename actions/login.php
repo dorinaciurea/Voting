034cf608d8dbeaeaa4e0e258e6b09cda1ee30c8a
dorinaciurea <147984146+dorinaciurea@users.noreply.github.com>
2025-01-16 11:12:05 +0200

    Modified dashboard

--- actions/login.php
+++ actions/login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include('connect.php');
 
 $username=$_POST['username'];
@@ -10,6 +11,26 @@ $sql = "Select * from `userdata` where username='$username' and phonenumber='$ph
 
 $result=mysqli_query($con,$sql);
 if(mysqli_num_rows($result)>0){
-  
+  $sql="Select username,photo,votes,id from `userdata` where standard='group'";
+  $resultgroup=mysqli_query($con,$sql);
+  if(mysqli_num_rows($resultgroup)>0){
+    $groups=mysqli_fetch_all($resultgroup,MYSQLI_ASSOC);
+    $_SESSION['groups']=&$groups;
+  }
+  $data=mysqli_fetch_array($result);
+  $_SESSION['id']=$data['id'];
+  $_SESSION['status']=$data['status'];
+  $_SESSION['data']=$data;
+
+  echo '<script>
+  window.location="../partials/dashboard.php";
+  </script>';
+
+}else{
+  echo '<script>
+  alert("Invalid credentials");
+  window.location="../";
+  </script>';
 }
+
 ?>
---
+++ partials/dashboard.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Voting System-Dashboard</title>
+
+   <!--Bootstrap CSS Link --> 
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="bg-info text-light">
+  <div class="container my-5">
+    <a href="../"><button class="btn btn-dark text-light px-3">Back</button></a>
+    <a href="logout.php"><button class="btn btn-dark text-light px-3">Logout</button></a>
+    <h1 class="my-3">Voting System</h1>
+    <div class="row my-5">
+      <div class="col-md-7">
+        <!--Groups--> 
+        <div class="row">
+          <div class="col-md-4">
+            <img src="" alt="Group image">
+          </div>
+          <div class="col-md-8">
+            <strong class="text-dark h5">Group name:</strong><br>
+            <strong class="text-dark h5">Votes:</strong><br>
+          </div>
+        </div>
+        <hr style="color:black">
+      </div>
+      <div class="col-md-5"></div>
+      <!--User Profile-->
+        <img src="" alt="User image">
+        <br>
+        <br>
+        <strong class="text-dark h5">Name:</strong><br>
+        <strong class="text-dark h5">Phone number:</strong><br>
+        <strong class="text-dark h5">Status:</strong><br>
+      </div>
+  </div>
+  </div>
+</body>
+</html>
